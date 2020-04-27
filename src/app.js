c5337ea31a76e080ab2bf1d94284347bfbec9a33
Piotr Kaminski <piterwk1@.wp.pl>
2020-04-27 12:18:04 +0200

    add validating data

--- src/app.js
+++ src/app.js
@@ -5,9 +5,10 @@ import { FleetDataService } from './services/fleet-data-service.js';
 
 let dataService = new FleetDataService(); // eslint-disable-line
 dataService.loadData(fleet);
-
 // console.log(dataService.cars);
 
-for (const car of dataService.cars) console.log(car.license);
+// for (const car of dataService.cars) console.log(car.latLong);
 
-for (const drone of dataService.drones) console.log(drone.license);
+// for (const drone of dataService.drones) console.log(drone.license);
+
+for (const e of dataService.errors) console.log(e.message);
--- src/services/fleet-data-service.js
+++ src/services/fleet-data-service.js
@@ -1,5 +1,6 @@
 import { Car } from '../classes/car.js';
 import { Drone } from '../classes/drone.js';
+import { DataError } from './data-errors.js';
 
 export class FleetDataService {
   constructor() {
@@ -10,33 +11,93 @@ export class FleetDataService {
 
   loadData(fleet) {
     for (const data of fleet) {
-      // eslint-disable-next-line default-case
       switch (data.type) {
         case 'car':
-          const car = this.loadCar(data); // eslint-disable-line
-          this.cars.push(car);
-          //   this.cars.push(data);
+          if (this.validateCarData(data)) {
+            const car = this.loadCar(data);
+            if (car) this.cars.push(car);
+            //   this.cars.push(data);
+          } else {
+            const e = new DataError('Invalid car data', data);
+            this.errors.push(e);
+          }
           break;
         case 'drone':
-            const drone = this.loadDrone(data); // eslint-disable-line
-          this.cars.push(drone);
-          //   this.drones.push(data);
+          if (this.validateDroneData(data)) {
+            const drone = this.loadDrone(data);
+            this.drones.push(drone);
+            //   this.drones.push(data);
+          } else {
+            const e = new DataError('Invalid drone data', data);
+            this.errors.push(e);
+          }
           break;
+        default: {
+          const e = new DataError('Invalid vehicle type', data);
+          this.errors.push(e);
+        }
       }
     }
   }
 
+  // eslint-disable-next-line class-methods-use-this
   loadCar(car) {
-    const c = new Car(car.license, car.model, car.latLong);
-    c.make = car.make;
-    c.miles = car.miles;
-    return c;
+    try {
+      const c = new Car(car.license, car.model, car.latLong);
+      c.make = car.make;
+      c.miles = car.miles;
+      return c;
+    } catch (e) {
+      this.errors.push(new DataError('error loading car', car));
+    }
+    return null;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   loadDrone(drone) {
-    const d = new Drone(drone.license, drone.model, drone.latLong);
-    d.airTimeHours = drone.airTimeHours;
-    d.miles = drone.base;
-    return d;
+    try {
+      const d = new Drone(drone.license, drone.model, drone.latLong);
+      d.airTimeHours = drone.airTimeHours;
+      d.base = drone.base;
+      return d;
+    } catch (e) {
+      this.errors.push(new DataError('error loading drone', drone));
+    }
+    return null;
+  }
+
+  validateCarData(car) {
+    const requiredProps = 'license model latLong miles make'.split(' ');
+    let hasErrors = false;
+
+    for (const field of requiredProps) {
+      if (!car[field]) {
+        this.errors.push(new DataError(`invalid field ${field}`, car));
+        hasErrors = true;
+      }
+    }
+    if (Number.isNaN(Number.parseFloat(car.miles))) {
+      this.errors.push(new DataError('invalid milage', car));
+      hasErrors = true;
+    }
+    return !hasErrors;
+  }
+
+  validateDroneData(drone) {
+    const requiredProps = 'license model latLong airTimeHours base'.split(' ');
+    let hasErrors = false;
+
+    for (const field of requiredProps) {
+      if (!drone[field]) {
+        this.errors.push(new DataError(`invalid field ${field}`, drone));
+        hasErrors = true;
+      }
+    }
+
+    if (drone.base !== 'New York') {
+      this.errors.push(new DataError('invalid base', drone));
+      hasErrors = true;
+    }
+    return !hasErrors;
   }
 }

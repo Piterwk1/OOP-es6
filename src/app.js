1d7eb29279d4756056f942d090fed59c68da4f4f
Piotr Kaminski <piterwk1@.wp.pl>
2020-04-27 08:43:11 +0200

    add func loadCar and Drone to fleet-data-service.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ import { Drone } from './classes/drone.js';
 import { fleet } from './fleet-data.js';
 import { FleetDataService } from './services/fleet-data-service.js';
 
-const dataService = new FleetDataService();
+let dataService = new FleetDataService(); // eslint-disable-line
 dataService.loadData(fleet);
-console.log(dataService.cars);
+
+// console.log(dataService.cars);
+
+for (const car of dataService.cars) console.log(car.license);
+
+for (const drone of dataService.drones) console.log(drone.license);
--- src/services/fleet-data-service.js
+++ src/services/fleet-data-service.js
@@ -5,6 +5,7 @@ export class FleetDataService {
   constructor() {
     this.cars = [];
     this.drones = [];
+    this.errors = [];
   }
 
   loadData(fleet) {
@@ -12,12 +13,30 @@ export class FleetDataService {
       // eslint-disable-next-line default-case
       switch (data.type) {
         case 'car':
-          this.cars.push(data);
+          const car = this.loadCar(data); // eslint-disable-line
+          this.cars.push(car);
+          //   this.cars.push(data);
           break;
         case 'drone':
-          this.drones.push(data);
+            const drone = this.loadDrone(data); // eslint-disable-line
+          this.cars.push(drone);
+          //   this.drones.push(data);
           break;
       }
     }
+  }
+
+  loadCar(car) {
+    const c = new Car(car.license, car.model, car.latLong);
+    c.make = car.make;
+    c.miles = car.miles;
+    return c;
+  }
+
+  loadDrone(drone) {
+    const d = new Drone(drone.license, drone.model, drone.latLong);
+    d.airTimeHours = drone.airTimeHours;
+    d.miles = drone.base;
+    return d;
   }
 }
